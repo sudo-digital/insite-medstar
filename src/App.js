c06807a25a078925148699a82b8e9bb6dd7cce3d
paulballas <paul.ballas@gmail.com>
2018-04-17 11:25:58 -0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import OurWork from './components/OurWork';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='app'>
+        <Header />
+        <Router>
+          <div className='body'>
+            <Route exact path="/" component={Home} />
+            <Route path="/our-work" component={OurWork} />
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/OurWork.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class OurWork extends React.Component {
+
+  render() {
+    return (
+      <div>Our Work page!</div>
+    )
+  }
+}
+
+export default OurWork;
