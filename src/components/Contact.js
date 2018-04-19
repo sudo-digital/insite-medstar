3c2ed2d419b206dcb569e80362e365490a9331dc
paulballas <paul.ballas@gmail.com>
2018-04-19 15:46:42 -0600

    contact page and modal on home hero cta

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,48 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <div>Contact page!</div>
+      <div className='container'>
+        <div className='pad-container'>
+          <div className='row'>
+            <div className='col-md-10 col-md-offset-1 col-xs-12'>
+              <h2>Contact us</h2>
+              <p>Fill out the form below and we'll get back to you soon.</p>
+            </div>
+          </div>
+          <div className='space-2' />
+          <div className='row'>
+            <div className='col-md-10 col-md-offset-1 col-xs-12'>
+              <form action="https://formspree.io/paul@sudodigital.com" method='POST'>
+                <div className='row'>
+                  <div className='col-md-6 col-xs-12'>
+                    <div className='form-item'>
+                      <label>Name</label>
+                      <input type='text' placeholder='John Smith' name='name' />
+                    </div>
+                  </div>
+                  <div className='col-md-6 col-xs-12'>
+                    <div className='form-item'>
+                      <label>Email</label>
+                      <input type='text' placeholder='john@email.com' name='email' />
+                    </div>
+                  </div>
+                  <div className='col-xs-12'>
+                    <div className='form-item'>
+                      <label>Message</label>
+                      <textarea placeholder='Message...' name='message' rows='5' />
+                    </div>
+                  </div>
+                  <div className='col-xs-12 end-xs'>
+                    <div className='form-item'>
+                      <input type='submit' name='Submit' />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import Modal from '../Modal'
 
 class HomeHero extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  openModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  closeModal = () => {
+    this.setState({ showModal:false })
+  }
+
+  renderModal = () => {
+    const form = (
+      <form action="https://formspree.io/paul@sudodigital.com" method='POST'>
+        <div className='row'>
+          <div className='col-md-6 col-xs-12'>
+            <div className='form-item'>
+              <label>Name</label>
+              <input type='text' placeholder='John Smith' name='name' />
+            </div>
+          </div>
+          <div className='col-md-6 col-xs-12'>
+            <div className='form-item'>
+              <label>Email</label>
+              <input type='text' placeholder='john@email.com' name='email' />
+            </div>
+          </div>
+          <div className='col-xs-12'>
+            <div className='form-item'>
+              <label>Message</label>
+              <textarea placeholder='Message...' name='message' rows='5' />
+            </div>
+          </div>
+          <div className='col-xs-12 end-xs'>
+            <div className='form-item'>
+              <input className='m-b-0' type='submit' name='Submit' />
+            </div>
+          </div>
+        </div>
+      </form>
+    )
+    if(this.state.showModal) {
+      return <Modal title={'Partner with us'} body={form} close={this.closeModal} />
+    } else {
+      return null
+    }
+  }
 
   render() {
     return (
       <div className='home-hero'>
+        { this.renderModal() }
         <img
           className='img-bg'
           src='/images/placeholder-bg.png'
@@ -21,7 +75,7 @@ class HomeHero extends React.Component {
               </div>
               <div className='col-md-8 col-xs-12'>
                 <h3 className='type-white'>Supporting schools with comprehensive approaches for mental wellness.</h3>
-                <a className='btn-default btn-tertiary'>PARTNER WITH US</a>
+                <a onClick={this.openModal} className='btn-default btn-tertiary'>PARTNER WITH US</a>
               </div>
             </div>
           </div>
