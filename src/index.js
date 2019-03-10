86d2be184cb93103aaa5d8409f17ae74f7dde6e1
paulballas <paul.ballas@gmail.com>
2019-03-10 12:29:49 -0600

    remove css from index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
-import './mapbox.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
