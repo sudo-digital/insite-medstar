5c044fa2b9c0d4cb9bce937024b3b9e27147cb23
paulballas <paul.ballas@gmail.com>
2019-10-27 10:32:09 -0600

    teacher well-being workbook download

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -16,7 +16,7 @@ class Resources extends React.Component {
         </div>
       )
     })
-  );    
+  );
 
   render() {
     return (
@@ -30,17 +30,28 @@ class Resources extends React.Component {
               </div>
             </div>
             <div className='row m-t-1'>
-              <div className='col-md-4 col-xs-12'>
+              <div className='col-md-6 m-b-2 col-xs-12'>
                 <h4>Resources for Parents</h4>
                 {this.renderResources('parents')}
               </div>
-              <div className='col-md-4 col-xs-12'>
+              <div className='col-md-6 m-b-2 col-xs-12'>
                 <h4>Resources for Kids & Teens</h4>
                 {this.renderResources('kids')}
               </div>
-              <div className='col-md-4 col-xs-12'>
+              <div className='col-md-6 col-xs-12'>
                 <h4>Resources for Specific Topics</h4>
                 {this.renderResources('topics')}
+              </div>
+              <div className='col-md-6 col-xs-12'>
+                <h4>Resources for Educators</h4>
+                  <div className='block'>
+                    <a
+                      download
+                      className='resource-link'
+                      src='/resources/teacher-well-being-workbook-10-28-18.pdf'
+                      href='/resources/teacher-well-being-workbook-10-28-18.pdf'>Teacher Well-Being Workbook
+                    </a>
+                  </div>
               </div>
             </div>
           </div>
