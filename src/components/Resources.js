d916c8327b3c4a5d23fe27d712f9d69d99410bc2
paulballas <paul.ballas@gmail.com>
2019-11-18 21:55:37 -0700

    updated logos and new resources page

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -6,11 +6,14 @@ class Resources extends React.Component {
   renderResources = (group) => (
     resources.resources[group].map((r) => {
       return (
-        <div key={r.id} className='block'>
+        <div
+          key={r.id}
+          className='block'
+          >
           <a
             target='_blank'
             rel="noopener noreferrer"
-            className='resource-link'
+            className='resource-link large'
             href={r.link}>{r.name}
           </a>
         </div>
@@ -24,35 +27,68 @@ class Resources extends React.Component {
         <div className='container'>
           <div className='pad-container-sm'>
             <div className='row m-b-3'>
-              <div className='col-xs-12'>
-                <h3>Resources</h3>
-                <p>General resources for parents, kids, teens, and specific topics:</p>
+              <div className='col-xs-12 m-b-2'>
+                <h2>Resources</h2>
+                <p>General resources for parents, kids, teens, and specific topics.</p>
               </div>
             </div>
-            <div className='row m-t-1'>
-              <div className='col-md-6 m-b-2 col-xs-12'>
-                <h4>Resources for Parents</h4>
+            <div className='row middle-xs m-t-1'>
+              <div className='col-md-4 m-b-2 col-xs-12'>
+                <img
+                  src='/images/resources/resources-parents.png'
+                  className='full'
+                  alt='Parents'
+                  />
+              </div>
+              <div className='col-md-8 m-b-2 col-xs-12'>
+                <h3>Resources for Parents</h3>
                 {this.renderResources('parents')}
               </div>
-              <div className='col-md-6 m-b-2 col-xs-12'>
-                <h4>Resources for Kids & Teens</h4>
+
+              <div className='col-md-4 m-b-2 col-xs-12'>
+                <img
+                  src='/images/resources/resources-teens.png'
+                  className='full'
+                  alt='Parents'
+                  />
+              </div>
+              <div className='col-md-8 m-b-2 col-xs-12'>
+                <h3>Resources for Kids & Teens</h3>
                 {this.renderResources('kids')}
               </div>
-              <div className='col-md-6 col-xs-12'>
-                <h4>Resources for Specific Topics</h4>
-                {this.renderResources('topics')}
+
+              <div className='col-md-4 m-b-2 col-xs-12'>
+                <img
+                  src='/images/resources/resources-educators.png'
+                  className='full'
+                  alt='Parents'
+                  />
               </div>
-              <div className='col-md-6 col-xs-12'>
-                <h4>Resources for Educators</h4>
+
+              <div className='col-md-8 m-b-2 col-xs-12'>
+                <h3>Resources for Educators</h3>
                   <div className='block'>
                     <a
                       download
-                      className='resource-link'
+                      className='resource-link download'
                       src='/resources/teacher-well-being-workbook-10-28-18.pdf'
-                      href='/resources/teacher-well-being-workbook-10-28-18.pdf'>Teacher Well-Being Workbook
+                      href='/resources/teacher-well-being-workbook-10-28-18.pdf'><img src='/images/icons/download.svg' alt='download' />Teacher Well-Being Workbook
                     </a>
                   </div>
               </div>
+
+              <div className='col-md-4 m-b-2 col-xs-12'>
+                <img
+                  src='/images/resources/resources-topics.png'
+                  className='full'
+                  alt='Parents'
+                  />
+              </div>
+              <div className='col-md-8 m-b-2 col-xs-12'>
+                <h3>Resources for Specific Topics</h3>
+                {this.renderResources('topics')}
+              </div>
+
             </div>
           </div>
         </div>
